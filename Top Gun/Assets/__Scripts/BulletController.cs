c60aa02c9731511c11114309659bd4a449b0b24f
Usman Sattar <G00345816@gmit.ie>
2019-10-22 16:55:36 +0100

    Players and Enemies Created

---
+++ Top Gun/Assets/__Scripts/BulletController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    // player fires on space bar
+    // == fields ==
+    [SerializeField]
+    private Bullet bulletPrefab;
+
+    [SerializeField]
+    private float bulletSpeed = 5f;
+
+    [SerializeField]
+    private float firingRate = 0.4f;
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if( Input.GetKeyDown(KeyCode.Space))
+        {
+            // start firing
+            InvokeRepeating("Shoot", 0f, firingRate);
+        }
+        if(Input.GetKeyUp(KeyCode.Space))
+        {
+            // stop firing
+            CancelInvoke("Shoot");
+        }
+    }
+
+    private void Shoot()
+    {
+        Bullet bullet = Instantiate(bulletPrefab);
+        bullet.transform.position = transform.position;
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.velocity = Vector2.right * bulletSpeed;
+    }
+
+}
+
---
+++ Top Gun/Assets/__Scripts/GameBoundaries.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameBoundaries : MonoBehaviour
+{
+    // need to keep the player within the main camera
+    //== fields ==
+    public Camera mainCamera;
+
+    private Vector2 screenBounds;
+
+    private float objectWidth, objectHeight;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        // find the screen bounds and convert to unity 
+        screenBounds = mainCamera.ScreenToWorldPoint(
+                            new Vector3(Screen.width,
+                                        Screen.height,
+                                        mainCamera.transform.position.z));
+
+        objectWidth = gameObject.GetComponent<SpriteRenderer>().bounds.extents.x;
+        objectHeight = gameObject.GetComponent<SpriteRenderer>().bounds.extents.y;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 viewPos = transform.position;
+
+        viewPos.x = Mathf.Clamp(viewPos.x,
+                                screenBounds.x * -1 + objectWidth,
+                                screenBounds.x - objectWidth);
+        viewPos.y = Mathf.Clamp(viewPos.y,
+                                screenBounds.y * -1 + objectHeight,
+                                screenBounds.y - objectHeight);
+        transform.position = viewPos;
+    }
+
+}
