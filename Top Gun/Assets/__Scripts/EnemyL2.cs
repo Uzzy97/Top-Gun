b165fa29324a4a1c586fafe5429c45e736436e35
Usman Sattar <G00345816@gmit.ie>
2019-11-24 15:22:11 +0000

    Level 2 in Progress

---
+++ Top Gun/Assets/__Scripts/EnemyL2.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyL2 : MonoBehaviour
+{
+    public Transform player;
+    public float moveSpeed = 5f;
+    private Rigidbody2D rg;
+    private Vector2 movement;
+    public int health =100;
+    public GameObject deathEffect;
+    public float speed;
+    public float stoppingDistance;
+    public float reatreatDistance;
+
+    private float timeBtwShots;
+    public float startTimeBtwShots;
+
+    public GameObject projectile;  
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rg = this.GetComponent<Rigidbody2D>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        timeBtwShots = startTimeBtwShots;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = player.position - transform.position;
+        //float angle = Mathf.Atan2(direction.y, direction.x)* Mathf.Rad2Deg;
+        //rg.rotation = angle;
+        direction.Normalize(); 
+        movement = direction;
+        if (Vector2.Distance(transform.position, player.position) > stoppingDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+        }
+        else if (Vector2.Distance(transform.position, player.position) < stoppingDistance && Vector2.Distance(transform.position, player.position) > reatreatDistance)
+        {
+            transform.position = this.transform.position;
+        }
+        else if (Vector2.Distance(transform.position, player.position) < stoppingDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, -speed * Time.deltaTime);
+
+        }
+
+        if (timeBtwShots <= 0)
+        {
+            Instantiate(projectile, transform.position, Quaternion.identity);
+            timeBtwShots = startTimeBtwShots;
+        }else{
+            timeBtwShots -= Time.deltaTime;
+        }
+    }
+
+    public void takeDamage(int damage )
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            Die();
+        }
+
+    }
+
+    void Die()
+    {
+        Instantiate(deathEffect,  transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+
+    private void FixedUpdate(){
+        moveChar(movement);
+    }
+
+    void moveChar(Vector2 direction){
+        rg.MovePosition((Vector2)transform.position + (direction * moveSpeed * Time.deltaTime));
+    }
+
+      private int scoreValue = 10;
+} 
---
+++ Top Gun/Assets/__Scripts/PlayerL2.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerL2 : MonoBehaviour
+{
+     // == fields ==
+    [SerializeField]    // adds this field to the Unity editor
+    private float playerSpeed = 5.0f;
+    public int health =100;
+    public GameObject deathEffect;
+    public Transform enemy;
+    private Rigidbody2D rbg;
+
+    // Start is called before the first frame update
+    void Start()    // initialisation
+    {
+        rbg = this.GetComponent<Rigidbody2D>();
+    }
+
+    // == private methods ==
+    private void Update()
+    {
+        Move();
+        Vector3 direction = enemy.position - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x)* Mathf.Rad2Deg;
+        rbg.rotation = angle;
+        direction.Normalize(); 
+    }
+
+    private void Move()
+    {
+        // need to ensure that deltaX is frame rate independent
+        // use Time.deltaTime as a multiplier
+        // also add a speed multiplier for the player to get a good feel
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * playerSpeed;
+        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * playerSpeed;
+        // add the delta to the current position
+        var newXPos = transform.position.x + deltaX;
+        var newYPos = transform.position.y + deltaY;
+        // update the current position
+        transform.position = new Vector2(newXPos, newYPos);
+    }
+
+    public void takeDamage(int damage )
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            Die();
+        }
+
+    }
+
+    void Die()
+    {
+        Instantiate(deathEffect,  transform.position, Quaternion.identity);
+         //  Destroy(gameObject);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    private int scoreValue = 10;
+}
